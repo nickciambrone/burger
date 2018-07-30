340544f79e12ddd3792bd376839dcd7a2576582f
Nick Ciambrone <nickciambrone@Nicks-MBP.fios-router.home>
2018-07-30 01:11:08 -0400

    asdfffffds

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,14 @@
 var mysql = require('mysql');
-
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL){
+connection=mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+    connection = mysql.createConnection({
     host:'localhost',
     user:'root',
     password:'root',
     database:'burger_db'
 })
+}
 module.exports=connection
